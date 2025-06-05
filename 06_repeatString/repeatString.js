ece9c10f0fd34a3d04abf0fd61987bf73ef957f5
thagan-arca <214645868+thagan-arca@users.noreply.github.com>
2025-06-05 10:08:26 +0100

    Exercises 2-6 completed, ready for commit.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,18 @@
 const repeatString = function() {
+    const str = arguments[0];
+    const num = arguments[1];
+
+    let response = '';
+
+    if (num < 0) {
+        return 'ERROR';
+    }
+
+    for (let i = 0; i < num; i++) {
+        response += str;
+    }
+
+    return response
 
 };
 
