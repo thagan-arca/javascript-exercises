b4f24f436fe2b8638842200951de856366786e3d
thagan-arca <214645868+thagan-arca@users.noreply.github.com>
2025-06-05 10:09:20 +0100

    Exercises 2-6 completed, ready for commit.

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -8,9 +8,15 @@
 */
 
 // Add your code right below, good luck!
+firstName = "Carlos"
+lastName = "Stevenson"
+thisYear = 1965
+birthYear = 1947
+age = thisYear - birthYear
 
+fullName = `${firstName} ${lastName}`
 
-
+greeting = `Hello! My name is ${fullName} and I am ${age} years old.`
 
 
 // Do not change this
